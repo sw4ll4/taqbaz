5658c10d7d9fbc0e1aefac53689138377840ea3d
sw4ll4 <saleh.bhzpour@gmail.com>
2025-06-16 11:32:43 +0330

    login in routes added

--- back end/app/routes/user.py
+++ back end/app/routes/user.py
@@ -18,5 +18,10 @@ def get_db():
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
     return Create_user(user, db)
 
-
-
+@router.post("/login", response_model= Token)
+def Login(user: Userlogin,db:Session = Depends(get_db)):
+    db_user = authenticate_user(db, user.username, user.password)
+    if not db_user:
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail = "invalid credentials")
+    access_token = create_access_token(data={"sub": db_user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
