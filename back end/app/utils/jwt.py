3b01657f221f54a2fd990e7bb41a971f7bef5ea3
sw4ll4 <saleh.bhzpour@gmail.com>
2025-06-16 13:37:25 +0330

    Update utils/jwt.py
    def get_current_user added.

--- back end/app/utils/jwt.py
+++ back end/app/utils/jwt.py
@@ -1,9 +1,16 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from app.database import Sessionlocal
+from app.models.user import User
+
 
 SECRET_KEY = "very_secret_key" # بعدش ببرش تو env
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl= "login")
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -11,3 +18,21 @@ def create_access_token(data: dict):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
 
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credential_exception = HTTPException(
+        status_code = status.HTTP_401_UNAUTHORIZED,
+        detail = "could not validate credential",
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+    except JWTError:
+        raise credential_exception
+    
+    db = Sessionlocal()
+    user = db.query(User).filter(User.username == username).first()
+    if user is None:
+        raise credential_exception
+    return user
